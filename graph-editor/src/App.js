8b62a51885881eefc4822c0c2f6e83c9ff58e3db
Yodyeam Autanuwat <meen41813@hotmail.com>
2018-06-25 12:09:05 +0700

    change lib to react-vis and modify create node func

--- graph-editor/src/App.js
+++ graph-editor/src/App.js
@@ -1,56 +1,67 @@
 import React, { Component } from 'react';
-import { Graph } from 'react-d3-graph';
 import './App.css';
+import Graph from 'react-graph-vis'
 
- let data = {
-  nodes: [{ id: '1',color: 'green',name:'Harry' }, { id: '2',color: 'green',name:'Sally' }, { id: '3',color: 'green',name:'Alice' }],
-  links: [{ source: '1', target: '2' }, { source: '1', target: '3' }]
+let graph = {
+  nodes: [
+      {id: 1,  label: 'Node 1'},
+      {id: 2, label: 'Node 2'},
+      {id: 3, label: 'Node 3'},
+      {id: 4, label: 'Node 4'},
+      {id: 5, label: 'Node 5'}
+    ],
+  edges: [
+      {from: 1, to: 2},
+      {from: 1, to: 3},
+      {from: 2, to: 4},
+      {from: 2, to: 5}
+    ]
 };
-let data2 = {}
-  
-const myConfig = {
-  nodeHighlightBehavior: true,
-  node: {
-      
-      size: 120,
-      highlightStrokeColor: 'blue',
-      labelProperty: 'name'
-  },
-  link: {
-      highlightColor: 'lightblue'
+const options = {
+    /*groups: {
+      test: {color:{background:'red'}}
+    },*/
+    layout: {
+      hierarchical: true
+    },
+    edges: {
+      color:{
+        color: "blue",
+        hover: "blue",
+        highlight: "yellow"
+      }
+    },
+    /*nodes: {
+      color:{
+        color: "blue",
+        hover: {
+          border: "blue"
+        },
+        highlight:{
+          border:"yellow"
+        } 
+      }
+    },*/
+    interaction:{
+      hover:true  
+    },
+    manipulation:{
+      enabled: true    
+    }
+};
+
+const events = {
+  select: function(event) {
+      var { nodes, edges } = event;
   }
 };
-const onClickNode = function(nodeId) {
-  window.alert('Clicked node ${nodeId}');
-};
 
-const onMouseOverNode = function(nodeId) {
-  window.alert(`Mouse over node ${nodeId}`);
-};
-
-const onMouseOutNode = function(nodeId) {
-  // window.alert(`Mouse out node ${nodeId}`);
-};
-
-const onClickLink = function(source, target) {
-  window.alert(`Clicked link between ${source} and ${target}`);
-};
-
-const onMouseOverLink = function(source, target) {
-  window.alert(`Mouse over in link between ${source} and ${target}`);
-};
-
-const onMouseOutLink = function(source, target) {
-  window.alert(`Mouse out link between ${source} and ${target}`);
-};
 class App extends Component {
   constructor(props){
     super(props)
     this.state= {
-      value: [data.nodes],
-      value2:[data.links],
+      graph: graph,
       textvalue : " ",
-      test:""
       
     }
     this.handleAddTodoItem = this.handleAddTodoItem.bind(this);
@@ -63,49 +74,74 @@ class App extends Component {
 
   }
   handleAddTodoItem(){
-    if (data.nodes[0].color=='green'){
-      data.nodes[0].color = 'red'
+
+    let newNode ={id:this.state.textvalue,label:this.state.textvalue}
+    let copy1 =this.state.graph.nodes.slice()
+    let copy2 =this.state.graph.edges.slice()
+    let check 
+    //console.log(graph.nodes[1])
+    for(let ele in copy1){
+      //  console.log(ele)
+      if ((JSON.stringify(newNode)) === JSON.stringify(copy1[ele])){
+
+        check = false
+        
+        break         
+      }
+      else{
+      
+        check = true
+        
+      }
     }
-    //this.state.value[0].push(newNode)  
+
+    if(check == true){
+      copy1.push(newNode)  
+      //console.log(this.state.graph.edges)
+      console.log(copy1)
       this.setState(
-        this.state
+        {graph:{nodes:copy1,edges:copy2}}
       )
-      this.state
+      
+      
+
+    }
+    
+   
+
+
+    
   
      
   }
   render() {
-    let show = document.getElementById("Canvas")
-    if(data.nodes.length == 0){
+    /*let show = document.getElementById("Canvas")
+    if(graph.nodes.length == 0){
       
     }
     else{
       //console.log(data.nodes.length)
-    }
-    let { value }  = this.state;
+    }*/
+
+   // console.log(value)
     return (
-       <div className="App">
+       <div id="App">
          
          <p className="App-intro"> NogDB Graph UI </p> 
          <div className="Top-Box" align="center">Limit</div>
          <p className="Display-msg">Displaying ... nodes, ... relationships. </p>
            <br/>
            <input type="text" placeholder="Inputhere" className="text" onChange={this.handleChange} />
+           
            <button id="Addnode-button" onClick={this.handleAddTodoItem}>Add node</button>
+           
            <button id="FullScreen-button" onClick={this.decrease}>Full screen</button>
            <button id="Clear-Canvas"> Clear Canvas </button>
 
-          <div id="Canvas" align="center" >Canvas area
-          
-            < Graph id="graph-id"
-              data={data}
-              config={myConfig}
-              onClickNode={onClickNode}
-              onClickLink={onClickLink}
-              onMouseOverNode={onMouseOverNode}
-              onMouseOutNode={onMouseOutNode}
-              onMouseOverLink={onMouseOverLink}
-              onMouseOutLink={onMouseOutLink}/>;
+          <div id="Canvas" align="center" style={{height: '600px'}}>Canvas area
+            <Graph graph={this.state.graph} options={this.state.options} events={this.state.events} />
+            {console.log(this.state.graph)}
+            
           </div> 
        </div>
     );
